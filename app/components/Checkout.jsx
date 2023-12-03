114317f7decbacc2e794f36a314f7d098019305f
NirwanArr <arrachmann@gmail.com>
2023-12-03 20:20:23 +0700

    firs Commit

--- app/components/Checkout.jsx
+++ app/components/Checkout.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { product } from "../libs/product";
+import Link from "next/link";
 
 const Checkout = () => {
   const [quantity, setQuantity] = useState(1);
+  const [paymentUrl, setPaymentUrl] = useState("");
 
   const decreaseQuantity = () => {
     setQuantity((prevState) => (quantity > 1 ? prevState - 1 : null));
@@ -12,11 +15,57 @@ const Checkout = () => {
   };
 
   const checkout = async () => {
-    alert("Checkout SNAP! 🌟")
+    // alert("Checkout SNAP! 🌟");
+    const data = {
+      id: product.id,
+      productName: product.name,
+      price: product.price,
+      quantity: quantity,
+    };
+
+    const response = await fetch("/api/tokenizer", {
+      method: "POST",
+      body: JSON.stringify(data)
+    });
+
+    const requestData = await response.json();
+    window.snap.pay(requestData.token);
   };
 
   const generatePaymentLink = async () => {
-    alert("Checkout Payment Link! 🔥")
+    // alert("Checkout Payment Link! 🔥");
+    const secret = process.env.NEXT_PUBLIC_SECRET;
+    const encodedSecret = Buffer.from(secret).toString('base64');
+    const basicAuth = `Basic ${encodedSecret}`;
+
+    let data = {
+      items_details: [
+        {
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          quantity: quantity,
+        }
+      ],
+      transaction_details: {
+        order_id: product.id,
+        gross_amount: product.price * quantity
+      }
+    };
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API}/v1/payment-links`, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": basicAuth
+      },
+      body: JSON.stringify(data)
+    });
+
+    const paymentLink = await response.json();
+    console.log(paymentLink);
+    setPaymentUrl(paymentLink.payment_url);
   };
 
   return (
@@ -58,6 +107,9 @@ const Checkout = () => {
       >
         Create Payment Link
       </button>
+      <div className="text-black underline italic">
+        <Link href={paymentUrl} target="_blank">Klik disini</Link>
+      </div>
     </>
   );
 };
